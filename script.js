9eba4060ae3672069aa38fc37a867b1350acdad0
DayanaraChuraco <cdchuraco@uce.edu.ec>
2024-11-21 11:20:27 -0500

    style: :art: style modification

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Aplicación estática cargada correctamente.");
+document.getElementById("miBoton").addEventListener("click", function() {
+    const mensajeElemento = document.getElementById("mensaje");
+    mensajeElemento.classList.add("fade"); 
+    mensajeElemento.innerText = " " + obtenerMensajeAleatorio();
+  });
+  
+  function obtenerMensajeAleatorio() {
+    const mensajes = [
+      "If we can pass the matter.",
+      "The only way to do great work is to love what you do.",
+      "Live as if you were going to die tomorrow. Learn as if you were going to live forever."
+    ];
+  
+    return mensajes[Math.floor(Math.random() * mensajes.length)];
+  }
+  
